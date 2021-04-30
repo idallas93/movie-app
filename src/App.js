ad1c9d0ebbe2b00b77610c1ef2dbe3743265e438
idallas93 <Dallas.Isaac1@gmail.com>
2021-04-30 10:15:14 -0400

    updated drag and drop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import MovieList from "./components/MovieList";
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -33,6 +32,21 @@ function App() {
     setFavoriteMovie(newFavoriteMovie);
   };
 
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    const items = Array.from(data);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setData(items);
+  }
+
+  function handleOnDragEnd2(result) {
+    if (!result.destination) return;
+    const items = Array.from(favoriteMovie);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setFavoriteMovie(items);
+  }
 
   if (!sort) {
     return (
@@ -47,16 +61,19 @@ function App() {
           ></SearchBox>
         </div>
         <button
+          id="sort-button"
           type="button"
           className="btn btn-light"
-          onClick={()=>{setSort(!sort)}}
+          onClick={() => {
+            setSort(!sort);
+          }}
         >
           Click to sort movies by release date
         </button>
 
         <div className="row">
           <div className="col-6">
-            <DragDropContext>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
               <Droppable droppableId="movies">
                 {(provided) => (
                   <ul
@@ -72,7 +89,10 @@ function App() {
                           index={index}
                         >
                           {(provided) => (
-                            <div className="image-container d-flex justify-content-start m-3">
+                            <div
+                              id="card-container"
+                              className="image-container d-flex justify-content-start m-3"
+                            >
                               <li
                                 {...provided.draggableProps}
                                 {...provided.dragHandleProps}
@@ -87,20 +107,20 @@ function App() {
                                 </div>
                                 <p>Title: {movie.Title}</p>
                                 <p>Release Year: {movie.Year}</p>
-
                               </li>
                             </div>
                           )}
                         </Draggable>
                       );
                     })}
+                    {provided.placeholder}
                   </ul>
                 )}
               </Droppable>
             </DragDropContext>
           </div>
           <div className="row d-flex align-items-center mt-4 mb-4"></div>
-          <DragDropContext>
+          <DragDropContext onDragEnd={handleOnDragEnd2}>
             <Droppable droppableId="movies">
               {(provided) => (
                 <ul
@@ -116,7 +136,10 @@ function App() {
                         index={index}
                       >
                         {(provided) => (
-                          <div className="image-container d-flex justify-content-start m-3">
+                          <div
+                            id="card-container"
+                            className="image-container d-flex justify-content-start m-3"
+                          >
                             <li
                               {...provided.draggableProps}
                               {...provided.dragHandleProps}
@@ -131,13 +154,13 @@ function App() {
                               </div>
                               <p>Title: {movie.Title}</p>
                               <p>Release Year: {movie.Year}</p>
-
                             </li>
                           </div>
                         )}
                       </Draggable>
                     );
                   })}
+                  {provided.placeholder}
                 </ul>
               )}
             </Droppable>
@@ -160,15 +183,16 @@ function App() {
         <button
           type="button"
           className="btn btn-light"
-          onClick={()=>{setSort(!sort)}}
-
+          onClick={() => {
+            setSort(!sort);
+          }}
         >
           Click to sort movies by release date
         </button>
 
         <div className="row">
           <div className="col-6">
-            <DragDropContext>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
               <Droppable droppableId="movies">
                 {(provided) => (
                   <ul
@@ -176,7 +200,61 @@ function App() {
                     {...provided.droppableProps}
                     ref={provided.innerRef}
                   >
-                    {data.sort((a, b) => a.Year.localeCompare(b.Year)).map((movie, index) => {
+                    {data
+                      .sort((a, b) => a.Year.localeCompare(b.Year))
+                      .map((movie, index) => {
+                        return (
+                          <Draggable
+                            key={movie.imdbID}
+                            draggableId={movie.imdbID}
+                            index={index}
+                          >
+                            {(provided) => (
+                              <div
+                                id="card-container"
+                                className="image-container d-flex justify-content-start m-3"
+                              >
+                                <li
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  ref={provided.innerRef}
+                                >
+                                  <img
+                                    src={movie.Poster}
+                                    alt={movie.Title}
+                                  ></img>
+                                  <div
+                                    onClick={() => addFavoriteMovie(movie)}
+                                    className="overlay d-flex align-items-center justify-content-center"
+                                  >
+                                    <AddFavorites />
+                                  </div>
+                                  <p>Title: {movie.Title}</p>
+                                  <p>Release Year: {movie.Year}</p>
+                                </li>
+                              </div>
+                            )}
+                          </Draggable>
+                        );
+                      })}
+                    {provided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div>
+          <div className="row d-flex align-items-center mt-4 mb-4"></div>
+          <DragDropContext onDragEnd={handleOnDragEnd2}>
+            <Droppable droppableId="movies">
+              {(provided) => (
+                <ul
+                  className="movies"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {favoriteMovie
+                    .sort((a, b) => a.Year.localeCompare(b.Year))
+                    .map((movie, index) => {
                       return (
                         <Draggable
                           key={movie.imdbID}
@@ -184,7 +262,10 @@ function App() {
                           index={index}
                         >
                           {(provided) => (
-                            <div className="image-container d-flex justify-content-start m-3">
+                            <div
+                              id="card-container"
+                              className="image-container d-flex justify-content-start m-3"
+                            >
                               <li
                                 {...provided.draggableProps}
                                 {...provided.dragHandleProps}
@@ -205,50 +286,7 @@ function App() {
                         </Draggable>
                       );
                     })}
-                  </ul>
-                )}
-              </Droppable>
-            </DragDropContext>
-          </div>
-          <div className="row d-flex align-items-center mt-4 mb-4"></div>
-          <DragDropContext>
-            <Droppable droppableId="movies">
-              {(provided) => (
-                <ul
-                  className="movies"
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {favoriteMovie.map((movie, index) => {
-                    return (
-                      <Draggable
-                        key={movie.imdbID}
-                        draggableId={movie.imdbID}
-                        index={index}
-                      >
-                        {(provided) => (
-                          <div className="image-container d-flex justify-content-start m-3">
-                            <li
-                              {...provided.draggableProps}
-                              {...provided.dragHandleProps}
-                              ref={provided.innerRef}
-                            >
-                              <img src={movie.Poster} alt={movie.Title}></img>
-                              <div
-                                onClick={() => addFavoriteMovie(movie)}
-                                className="overlay d-flex align-items-center justify-content-center"
-                              >
-                                <AddFavorites />
-                              </div>
-                              <p>Title: {movie.Title}</p>
-                              <p>Release Year: {movie.Year}</p>
-
-                            </li>
-                          </div>
-                        )}
-                      </Draggable>
-                    );
-                  })}
+                  {provided.placeholder}
                 </ul>
               )}
             </Droppable>
@@ -260,3 +298,4 @@ function App() {
 }
 
 export default App;
+
